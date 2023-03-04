cf2586cf6806a92fd061bed123455cd4ec7464f4
Abhishek Patel <46045014+abhishekpatel946@users.noreply.github.com>
2023-03-04 16:13:49 +0530

    added chapter-08-slices in go

---
+++ 08-slices/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	fmt.Println("Welcome to slices in go")
+
+	var fruitList = []string{"Apple", "Tomato"}
+
+	fmt.Printf("Type of fruitList is %T", fruitList)
+	fmt.Println("Values are:", fruitList)
+	fmt.Println("length of fruitList:", len(fruitList))
+
+	fruitList = append(fruitList, "Mango")
+	fruitList = append(fruitList, "Banana", "Coconut")
+
+	fmt.Println(fruitList)
+	fmt.Println(len(fruitList))
+
+	fruitList = append(fruitList[1:])
+	fmt.Println(fruitList)
+	fmt.Println(len(fruitList))
+
+	fruitList = append(fruitList[1:3])
+	fmt.Println(fruitList)
+	fmt.Println(len(fruitList))
+
+	// "make" for raw memory management
+	highScores := make([]int, 4)
+
+	highScores[0] = 234
+	highScores[1] = 897
+	highScores[2] = 654
+	highScores[3] = 987
+	// highScores[4] = 777
+
+	fmt.Println(highScores)
+
+	highScores = append(highScores, 777, 321, 555)
+
+	fmt.Println(highScores)
+	sort.Ints(highScores)
+	fmt.Println(highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+
+}
