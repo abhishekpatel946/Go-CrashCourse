6b2abb6f1a9527b851acf9a06a86b24c5c02924f
Abhishek Patel <46045014+abhishekpatel946@users.noreply.github.com>
2023-03-04 18:50:32 +0530

    added the chapter-11-ifelse in go

---
+++ 11-ifelse/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Welcome to if-else in go")
+
+	if num := 3; num < 10 {
+		fmt.Println("Number is lessar < 10")
+	} else {
+		fmt.Println("Number is greater > 10")
+	}
+
+}
