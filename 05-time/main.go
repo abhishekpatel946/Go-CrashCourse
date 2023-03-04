d5b33f05e09e2a17ba0b3152e99af5f3718dd7a6
Abhishek Patel <46045014+abhishekpatel946@users.noreply.github.com>
2023-03-04 13:32:48 +0530

    added chapter-05-time in go

---
+++ 05-time/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("Welcom to time study of golang")
+
+	currentTime := time.Now()
+	fmt.Println("Curren time is:", currentTime)
+
+	fmt.Println(currentTime.Format("01-02-2006 01:04:05 Monday"))
+
+	createdDate := time.Date(2020, time.January, 10, 12, 23, 0, 0, time.UTC)
+	fmt.Println(createdDate)
+	fmt.Println(createdDate.Format("01-02-2006 Monday"))
+}
